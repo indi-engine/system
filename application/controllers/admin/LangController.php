f22cd1a71083d603d4e49729b97bc1b88133385b
Pavel Perminov <pavel.perminov.23@gmail.com>
2022-06-25 16:51:37 +0300

    LangController->onBeforeCellSave():  first available language is selected by default in source-langs combobox

--- application/controllers/admin/LangController.php
+++ application/controllers/admin/LangController.php
@@ -75,21 +75,27 @@ class Admin_LangController extends Indi_Controller_Admin {
         // If we're going to add new translation
         if ($value == 'qy') {
 
+            // Prepare WHERE clause for source languages list
+            $langId_where = '`id` != "' . t()->row->id . '" AND `' . $cell . '` = "y"';
+
             // Create phantom `langId` field
             $langId_combo = m('Field')->new([
-                'title' => 'asdasd',
                 'alias' => 'langId',
                 'columnTypeId' => 'INT(11)',
                 'elementId' => 'combo',
                 'storeRelationAbility' => 'one',
                 'relation' => 'lang',
-                'filter' => '`id` != "' . t()->row->id . '" AND `' . $cell . '` = "y"',
+                'filter' => $langId_where,
                 'mode' => 'hidden',
                 'defaultValue' => 0
             ]);
 
             // Append to fields list
             m()->fields()->append($langId_combo);
+
+            // Setup a value for phantom `langId` field for the current row, as for now
+            // it's the only way to make sure it to be a selected value in prompt's combobox
+            t()->row->langId = m('lang')->row($langId_where, 'move')->id;
 
             // Build config for langId-combo
             $combo = ['fieldLabel' => '', 'allowBlank' => 0] + t()->row->combo('langId');
