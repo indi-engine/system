13363c622921edd7e709b42f088accbeb711ae16
Pavel Perminov <pavel.perminov.23@gmail.com>
2023-10-10 09:20:58 +0000

    l10n of `changeLog`.`(was|now)` fields is now moved:  -  *_Queue_L10n_{AdminUi => AdminCustomData}->chunk()

--- library/Indi/Queue/L10n/AdminCustomData.php
+++ library/Indi/Queue/L10n/AdminCustomData.php
@@ -37,9 +37,36 @@ class Indi_Queue_L10n_AdminCustomData extends Indi_Queue_L10n_AdminUi {
         } else {
 
             // Create chunks
-            foreach (m('Entity')->all('`fraction` = "custom"', '`table` ASC') as $entityR)
+            foreach (m('Entity')->all('`fraction` = "custom" OR `table` = "changeLog"', '`table` ASC') as $entityR) {
+
+                /**
+                 * WHERE clause for `changeLog` values
+                 */
+                if ($entityR->table == 'changeLog') {
+
+                    // Get distinct `fieldId`-values
+                    $fieldIds = im(db()->query('SELECT DISTINCT `fieldId` FROM `changeLog`')->col());
+
+                    // Collect ids of applicable fields
+                    $fieldIdA = [];
+                    foreach (m('Field')->all(['`id` IN (0' . rif($fieldIds, ',$1') . ')',
+                        '(`l10n` = "y" OR `storeRelationAbility` != "none")'
+                    ]) as $fieldR)
+                        if ($fieldR->l10n == 'y' || $fieldR->rel()->titleField()->l10n == 'y')
+                            $fieldIdA []= $fieldR->id;
+
+                    // Prepend `fieldId`-clause
+                    $where = '`fieldId` IN (0' . rif(im($fieldIdA), ',$1') . ')';
+
+                // Else no WHERE clause should be used
+                } else {
+                    $where = false;
+                }
+
+                // Foreach field - append chunk
                 foreach ($entityR->nested('field', ['where' => '`l10n` = "y" AND IFNULL(`relation`, 0) != "6"']) as $fieldR)
-                    $this->appendChunk($queueTaskR, $entityR, $fieldR);
+                    $this->appendChunk($queueTaskR, $entityR, $fieldR, $where ? [$where] : []);
+            }
 
             // Order chunks to be sure that all dependen fields will be processed after their dependencies
             $this->orderChunks($queueTaskR->id);
