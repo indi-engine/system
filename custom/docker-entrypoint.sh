c27b014eca3161ebbe92fa24e2f907b43cf0b5e6
Pavel Perminov <pavel.perminov.23@gmail.com>
2024-07-08 11:41:23 +0300

    1.Dockerfile: added opendkim install 2.added custom/compose/opendkim.conf 3.custom/docker-compose.yml  - added services.apache.volumes.'- ./compose/opendkim.conf:/etc/opendkim.conf' 4.custom/docker-entrypoint.sh  - added opendkim setup

--- custom/docker-entrypoint.sh
+++ custom/docker-entrypoint.sh
@@ -41,15 +41,50 @@ if [ -f "$pid_file" ]; then rm "$pid_file" && echo "Apache old pid-file removed"
 cp /usr/bin/mysql_client_binaries/* /usr/bin/
 
 # Obtain Let's Encrypt certificate, if LE_DOMAIN env is given:
-# 1.Start apache in background to make certbot challenge possible
-# 2.Obtain certificate
-# 3.Stop apache in backgrond
-# 4.Setup cron job for certificate renewal check
 if [[ ! -z "$LETS_ENCRYPT_DOMAIN" ]]; then
+
+  # 1.Start apache in background to make certbot challenge possible
+  # 2.Obtain certificate
+  # 3.Stop apache in backgrond
+  # 4.Setup cron job for certificate renewal check
   service apache2 start
   certbot --apache -n -d $LETS_ENCRYPT_DOMAIN -m $GIT_COMMIT_EMAIL --agree-tos -v
   service apache2 stop
   echo "0 */12 * * * certbot renew" | crontab -
+
+  # Also, configure postfix and opendkim to ensure outdoing emails deliverability
+  dkim="/etc/opendkim"
+  selector="mail"
+  maildomain=$LETS_ENCRYPT_DOMAIN
+  domainkeys="$dkim/keys/$maildomain"
+  priv="$domainkeys/$selector.private"
+  DNSname="$selector._domainkey.$maildomain"
+
+  # If trusted.hosts file does not yet exist - it means we're setting up opendkim for the very first time
+  # so add localhost to the list of trusted hosts and append changes to postfix config for it to invokate opendkim
+  # Note: if change LETS_ENCRYPT_DOMAIN and restart the container, postfix will keep using previous
+  # value of that env as mail domain
+  if [[ ! -f "$dkim/trusted.hosts" ]]; then
+    echo -e "127.0.0.1\nlocalhost"                 >> "$dkim/trusted.hosts"
+    sed -Ei "s~(myhostname\s*=)\s*.*~\1 $maildomain~" "/etc/postfix/main.cf"
+    echo "smtpd_milters = inet:localhost:8891"     >> "/etc/postfix/main.cf"
+    echo "non_smtpd_milters = inet:localhost:8891" >> "/etc/postfix/main.cf"
+  fi
+
+  # If private key file was not generated so far
+  if [[ ! -f $priv ]]; then
+
+    # Generate key files
+    mkdir -p $domainkeys
+    opendkim-genkey -D $domainkeys -s $selector -d $maildomain
+    chown opendkim:opendkim $priv
+    chown $user:$user "$domainkeys/$selector.txt"
+
+    # Setup key.table, signing.table and trusted.hosts files to be picked by opendkim
+    echo "$DNSname $maildomain:$selector:$priv"   >> "$dkim/key.table"
+    echo "*@$maildomain $DNSname"                 >> "$dkim/signing.table"
+    echo "*.$maildomain"                          >> "$dkim/trusted.hosts"
+  fi
 fi
 
 # Setup crontab
@@ -58,7 +93,8 @@ jobs='compose/crontab'
 sed -i "s~\$DOC~$DOC~" $jobs && crontab -u $user $jobs && $run 'git checkout '$jobs
 service cron start
 
-# Start postfix to be able to send emails via sendmail used by php
+# Start opendkim and postfix to be able to send DKIM-signed emails via sendmail used by php
+service opendkim start
 service postfix start
 
 # Start apache process
